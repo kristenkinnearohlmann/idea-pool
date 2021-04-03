94d75d0bddae6be90eef688f0ea64980cddfc8b7
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-04-02 20:37:47 -0500

    Drafted working copy of site, accessible via shotgun in WSL, with standard Hello World

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < Sinatra::Base
+    # extends Sinatra with methods and Rake tasks for dealing
+    # with SQL databses using ActiveRecord
+    register Sinatra::ActiveRecordExtension
+    
+    # set up sessions
+    # TODO: how to hide secret in the future
+    configure do
+        enable :sessions
+        set :session_secret, "vJHqHCgewkv7N9NE"
+    end
+
+    # set an alias for views so view files can be stored in 
+    # directories related to their model name
+    set :views, Proc.new { File.join(root, "../views/") }
+
+    get '/' do
+        erb :index
+    end
+end
--- config.ru
+++ config.ru
@@ -1,2 +1,7 @@
 # get environment configuration
 require_relative './config/environment'
+
+# allows use of PATCH and DELETE routes
+Rack::MethodOverride
+
+run ApplicationController
