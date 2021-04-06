7fc638adbc020ee74808aa2cbdc2691db9385114
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-04-05 23:30:46 -0500

    Added statement for private ideas, adding styling for private statement, links and site name

--- app/controllers/idea_controller.rb
+++ app/controllers/idea_controller.rb
@@ -7,7 +7,7 @@ class IdeaController < ApplicationController
     get '/ideas/:id' do
         @idea = Idea.find(params[:id])
 
-        if @idea.is_private == false || (@idea.is_private == true && Helpers.is_logged_in?(session) && Helpers.current_user(session).user = @idea.user)
+        if @idea.is_private == false || (@idea.is_private == true && Helpers.is_logged_in?(session) && Helpers.current_user(session) == @idea.user)
             erb :'ideas/show'
         else
             flash.next[:msg] = "You do not have access to that idea. Please select an idea below."
