ed1f398922926d7473abe531c06ccad6fab9e2f0
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-04-06 21:29:45 -0500

    Finished ideas show for read, projects show for read, updated projects model to reference ideas (will be 1 per)

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -4,4 +4,14 @@ class ProjectController < ApplicationController
         erb :'projects/index'
     end
 
+    get '/projects/:id' do
+        @project = Project.find(params[:id])
+        if @project.is_private == false || (@project.is_private == true && Helpers.is_logged_in?(session) && Helpers.current_user(session) == @project.user)
+            erb :'projects/show'
+        else
+            flash.next[:msg] = "You do not have access to that project. Please select a project below."
+            redirect '/projects'
+        end
+    end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
     belongs_to :user
     has_many :idea_projects
+    has_many :ideas, through: :idea_projects
 end
